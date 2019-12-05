f7f0d7ca0b422948ed2325c09c78d497e9e77a66
Gaute Berge <gauteab@Gautes-MacBook-Pro.local>
2019-12-05 02:00:41 +0100

    day3 part two

--- day3/Day3.hs
+++ day3/Day3.hs
@@ -1,55 +1,42 @@
--- import Data.List.Split
 import Data.List.Extra hiding (union)
-import Data.Either
-import Data.Maybe
--- import Data.List
+import qualified Data.Map.Strict as Map
 import Data.Set (member, toList, union, fromList, empty, Set, intersection)
 
-data Direction = V | H
-    deriving Show
-
-parse :: String -> [[(Char, Int)]]
-parse = fmap (fmap f . splitOn ",") . lines
-    where f (x:xs) = (x, read xs)
-          -- g ('R', i) = (H, i)
-          -- g ('L', i) = (H, -i)
-          -- g ('U', i) = (V, i)
-          -- g ('D', i) = (V, -i)
+parse :: String -> [String]
+parse = fmap (concatMap f . splitOn ",") . lines
+    where f (x:xs) = replicate (read xs) x
           
  
-move (x, y) (direction, steps) = 
+move :: (Int, Int) -> Char -> (Int, Int)
+move (x, y) direction = 
     case direction of
-      'R' -> ((x+steps, y), [(x',y) | x' <- [x+1..x+steps]])
-      'L' -> ((x-steps, y), [(x',y) | x' <- [x-steps..x-1]])
-      'U' -> ((x, y+steps), [(x,y') | y' <- [y+1..y+steps]])
-      'D' -> ((x, y-steps), [(x,y') | y' <- [y-steps..y-1]])
+      'R' -> (x+1, y)
+      'L' -> (x-1, y)
+      'U' -> (x, y+1)
+      'D' -> (x, y-1)
 
-wirePath :: [(Char, Int)] -> [(Int, Int)]
+wirePath :: String -> [(Int, Int)]
 wirePath = snd . foldl f init
-    where f (position, visited) x = let (newPosition, newVisited) = move position x in (newPosition ,visited ++ newVisited)
-          init = ((0,0), [])
+    where init = ((0,0), []) 
+          f (position, visited) direction = 
+            let newPosition = move position direction 
+             in (newPosition , newPosition : visited)
+          
 
--- wireIntersections :: [[(Int, Int)]] -> [(Int, Int)]
-wireIntersections paths = cross $ map fromList paths
-    where cross [x,y] = intersection x y
-
+solve1 :: [String] -> Int
 solve1 = minimum . map distance . toList . wireIntersections . map wirePath
     where distance (x,y) = abs x + abs y
+          wireIntersections paths = cross $ map fromList paths
+          cross [x,y] = intersection x y
 
--- solve2 :: [(Char, Int)] -> [(Int, Int)]
-solve2 xs = 
-    let paths = map wirePath xs
-        intersections = wireIntersections paths
-        p x = member (snd x) intersections
-        ps =  map (zip [0..]) paths
-        [x,y] = map (filter p) ps
-        result = filter (\(_,a) -> any (\(_,b) -> a==b) x) y
-     in result
+solve2 :: [String] -> Int
+solve2 paths = 
+    let [p1, p2] = map (Map.fromList . flip zip [1..] . reverse . wirePath) paths
+     in minimum $ map snd $ Map.toList $ Map.intersectionWith (+) p1 p2
 
 main :: IO ()
 main = do
     input <- readFile "input"
     let input' = parse input
-    print $  (solve1 input')
-    -- print $ solve1 input'
-    print $ solve2 input'
+    print $ solve1 input' -- 386
+    print $ solve2 input' -- 6484
